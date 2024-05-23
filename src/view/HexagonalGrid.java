4b7e002c094881ffd5d0a4971f543a9083a5ae0e
AllanDBD <123992798+AllanDBD@users.noreply.github.com>
2024-05-23 14:54:24 +0200

    init branche

---
+++ src/view/HexagonalGrid.java
@@ -0,0 +1,121 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class HexagonalGrid extends JFrame {
+    private BufferedImage image;
+    private JPanel imagePanel;
+    private ArrayList<Hexagon> hexagons;
+    private static final int HEX_SIZE = 30;
+    private Hexagon hoveredHexagon = null;
+
+    public HexagonalGrid() {
+        setTitle("Hexagonal Grid on Image");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 800);
+
+        hexagons = new ArrayList<>();
+        imagePanel = new ImagePanel();
+
+        try {
+            image = ImageIO.read(new File("theisland.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        imagePanel.setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
+        imagePanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                handleMouseClick(e);
+            }
+        });
+
+        imagePanel.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                handleMouseMove(e);
+            }
+        });
+
+        add(new JScrollPane(imagePanel));
+        createHexagonalGrid();
+    }
+
+    private void createHexagonalGrid() {
+        int radius = HEX_SIZE;
+        int horiz = (int) (Math.sqrt(3) * radius);
+        int vert = 2 * radius;
+        int[] indiceMaxLigne= {6,9,10,9,10,11,10,11,10,9,10,9,6};
+
+
+        for (int y = 0; y < 600; y += vert * 3 / 4) {
+            for (int x = 0; x < 532; x += horiz) {
+                int offsetX = (y / (vert * 3 / 4)) % 2 == 0 ? horiz / 2 : 0;
+                hexagons.add(new Hexagon(new Point(x + offsetX, y), radius));
+            }
+        }
+    }
+
+    private void drawHexagons(Graphics g) {
+        for (Hexagon hex : hexagons) {
+            if (hex == hoveredHexagon) {
+                g.setColor(Color.RED);  // Couleur de survol
+            } else {
+                g.setColor(Color.BLACK);  // Couleur par défaut
+            }
+            g.drawPolygon(hex.getHexagon());
+        }
+    }
+
+    private void handleMouseClick(MouseEvent e) {
+        Point clickedPoint = e.getPoint();
+        for (Hexagon hex : hexagons) {
+            if (hex.getHexagon().contains(clickedPoint)) {
+                System.out.println("Clicked on hexagon at: " + hex.getPosition());
+                break;
+            }
+        }
+    }
+
+    private void handleMouseMove(MouseEvent e) {
+        Point movedPoint = e.getPoint();
+        boolean found = false;
+        for (Hexagon hex : hexagons) {
+            if (hex.getHexagon().contains(movedPoint)) {
+                hoveredHexagon = hex;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            hoveredHexagon = null;
+        }
+        imagePanel.repaint();
+    }
+
+    private class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(image, 0, 0, null);
+            drawHexagons(g);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new HexagonalGrid().setVisible(true);
+            }
+        });
+    }
+}
+
