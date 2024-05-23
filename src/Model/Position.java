530905e2bc5384af53c9ae5dd5a32966fbc7fa22
AllanDBD <123992798+AllanDBD@users.noreply.github.com>
2024-05-23 12:00:00 +0200

    Méthode getNeighbors(Position position) dans Position.java

--- src/Model/Position.java
+++ src/Model/Position.java
@@ -1,5 +1,10 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.abs;
+
 public class Position {
     private int x;
     private int y;
@@ -23,5 +28,31 @@ public class Position {
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    public List<Position> getNeighbors(Position position) {
+        List<Position> neighbors = new ArrayList<Position>();
+        int pos_x = position.getX();
+        int pos_y = position.getY();
+
+        if(abs(pos_x-2) < indiceMaxLigne(pos_y))
+            neighbors.add(new Position(pos_x-2, pos_y));  //voisin de gauche
+
+        if(abs(pos_x+2) < indiceMaxLigne(pos_y))
+            neighbors.add(new Position(pos_x+2, pos_y));   //voisin de droite
+
+        if(abs(pos_x-1) < indiceMaxLigne(pos_y-1) && pos_y-1>=0)
+            neighbors.add(new Position(pos_x-1, pos_y-1));  //voisin en haut à gauche
+
+        if(abs(pos_x+1) < indiceMaxLigne(pos_y-1) && pos_y-1>=0)
+            neighbors.add(new Position(pos_x+1, pos_y-1));   //voisin en haut à droite
+
+        if(abs(pos_x-1) < indiceMaxLigne(pos_y+1) && pos_y+1>= nbLigne)   //voisin en bas à gauche
+            neighbors.add(new Position(pos_x-1, pos_y+1));
+
+        if(abs(pos_x+1) < indiceMaxLigne(pos_y+1) && pos_y+1>= nbLigne)   //voisin en bas à droite
+            neighbors.add(new Position(pos_x+1, pos_y+1));
+
+        return neighbors;
     }
 }
