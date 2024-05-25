1bf2041274c8e1237145db4cc771bb5c6befa26e
Pape Mamadou Diagne <66137298+Pape45@users.noreply.github.com>
2024-05-25 13:41:01 +0200

    feat: Following the previous modifications of HexagonalGrid, i've divided the main logic into his own file

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
+import javax.swing.SwingUtilities;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import view.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Account Creation");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(672, 620);
-        frame.setLocationRelativeTo(null);
-        JPanel panel =filtre1.create(frame);
-        frame.add(panel);
-        frame.setVisible(true);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new HexagonalGrid().setVisible(true);
+            }
+        });
     }
 }
