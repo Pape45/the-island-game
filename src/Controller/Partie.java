bf3137c67c3339d0cead26134d69a6267e61ecef
mellouliyoussef <162433850+mellouliyoussef@users.noreply.github.com>
2024-05-25 23:49:27 +0200

    partie

--- src/Controller/Partie.java
+++ src/Controller/Partie.java
@@ -1,6 +1,6 @@
 package Controller;
 import Model.*;
-import java.util.List;
+
 
 
 public class Partie {
@@ -9,6 +9,31 @@ public class Partie {
 
         PlateauJeu Plateau_de_jeu = new PlateauJeu();
         Plateau_de_jeu.setState_of_game(0);
+        Position buffer=new Position(0, 0);
+        Plateau_de_jeu.joueurs[0].explorateurs.get(0).setPosition(buffer);
+        buffer.setX(2);
+        buffer.setY(0);
+        Plateau_de_jeu.joueurs[0].explorateurs.get(0).setPosition(buffer);
+        buffer.setX(1);
+        buffer.setY(1);
+        Plateau_de_jeu.joueurs[1].explorateurs.get(0).setPosition(buffer);
+        buffer.setX(3);
+        buffer.setY(1);
+        Plateau_de_jeu.joueurs[1].explorateurs.get(0).setPosition(buffer);
+        buffer.setX(2);
+        buffer.setY(2);
+        Plateau_de_jeu.joueurs[2].explorateurs.get(0).setPosition(buffer);
+        buffer.setX(0);
+        buffer.setY(2);
+        Plateau_de_jeu.joueurs[2].explorateurs.get(0).setPosition(buffer);
+        buffer.setX(1);
+        buffer.setY(1);
+        Plateau_de_jeu.joueurs[3].explorateurs.get(0).setPosition(buffer);
+        buffer.setX(3);
+        buffer.setY(1);
+        Plateau_de_jeu.joueurs[3].explorateurs.get(0).setPosition(buffer);
+
+
         while(Plateau_de_jeu.getState_of_game()==0)
 
         {
