43dd0133045f817fd13b8191aee8dad7a3301437
Pape Mamadou Diagne <66137298+Pape45@users.noreply.github.com>
2024-05-26 14:54:00 +0200

    feat: Model files for running filtre_5

---
+++ src/Model/Tuile.java
@@ -0,0 +1,75 @@
+package Model;
+
+public class Tuile {
+
+    private int faceCachee;
+    private int faceVisible;
+    private int numeroTuile;
+    private String nom;
+    public Tuile(int faceCachee, int faceVisible) {
+        this.faceCachee=faceCachee;
+        this.faceVisible=faceVisible;
+
+    }
+
+    public int getFaceCachee() {
+        return faceCachee;
+    }
+
+    public void setFaceCachee(int faceCachee) {
+        this.faceCachee = faceCachee;
+    }
+
+    public int getFaceVisible() {
+
+        return faceVisible;
+    }
+
+    public void setFaceVisible(int faceVisible) {
+        this.faceVisible = faceVisible;
+    }
+
+    public void setNumeroTuile(int numeroTuile){
+        this.numeroTuile=numeroTuile;
+    }
+
+    public String toString() {
+        return "Tuile= "+ numeroTuile;
+    }
+}
+
+
+class TuilePlage extends Tuile {
+
+    public TuilePlage(int faceCachee, int faceVisible) {
+        super(faceCachee, faceVisible);
+
+    }
+
+}
+
+class TuileForet extends Tuile {
+
+    public TuileForet(int faceCachee, int faceVisible) {
+        super(faceCachee, faceVisible);
+
+    }
+
+}
+
+class TuileMontagne extends Tuile {
+    private String nom;
+    public TuileMontagne(int faceCachee, int faceVisible) {
+        super(faceCachee, faceVisible);
+
+    }
+
+    public void setTypeTuile(){
+        this.nom="Montagne";
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+}
