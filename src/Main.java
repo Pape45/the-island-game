44a560bcdc6487fd42fc7ccd5798c2d02c2527db
Pape Mamadou Diagne <66137298+Pape45@users.noreply.github.com>
2024-05-26 14:53:01 +0200

    feat: Main file for filtre_5

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 import javax.swing.SwingUtilities;
-
 import view.*;
+import Model.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new HexagonalGrid().setVisible(true);
+                HexagonalGrid hexagonalGrid = new HexagonalGrid();
+                hexagonalGrid.setVisible(true);
+
+                Init_Jeu jeu = new Init_Jeu();
+                Filtre5 filtre = new Filtre5();
+
+                int numeroTour = 1;
+                Joueur joueur = jeu.getJoueurs()[0];
+
+                filtre.filtre_5(hexagonalGrid, numeroTour, joueur);
             }
         });
     }
