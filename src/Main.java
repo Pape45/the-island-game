442f0371657ad3caccf6dd3e330a0f3723269101
Pape Mamadou Diagne <66137298+Pape45@users.noreply.github.com>
2024-05-28 11:40:22 +0200

    feat: minor changes

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,22 @@
 import javax.swing.SwingUtilities;
-import javax.swing.Timer;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import view.*;
 import Model.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                HexagonalGrid hexagonalGrid = new HexagonalGrid();
-                hexagonalGrid.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            HexagonalGrid hexagonalGrid = new HexagonalGrid();
+            hexagonalGrid.setVisible(true);
 
-                Init_Jeu jeu = new Init_Jeu();
-                Filtre5 filtre5 = new Filtre5();
-                Filtre6 filtre6 = new Filtre6();
+            Init_Jeu jeu = new Init_Jeu();
+            Filtre5 filtre5 = new Filtre5(hexagonalGrid);
+            Filtre6 filtre6 = new Filtre6(hexagonalGrid);
 
-                int numeroTour = 1;
-                Joueur joueur = jeu.getJoueurs()[0];
+            int numeroTour = 1;
+            Joueur joueur = jeu.getJoueurs()[0];
 
-                filtre5.filtre_5(hexagonalGrid, numeroTour, joueur);
-
-                // Test d'un message
-                filtre6.filtre_6(hexagonalGrid, "Message temp 1");
-
-                // TEst deuxième message
-                // Faudra donc gérer un time
-                Timer timer = new Timer(3000, new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        filtre6.filtre_6(hexagonalGrid, "Message temp 2");
-                    }
-                });
-                timer.setRepeats(false);
-                timer.start();
-            }
+            filtre5.updateTourAndPlayer(numeroTour, joueur);
+            filtre6.showTemporaryMessage("Message temp 1", 2000);
         });
     }
 }
