93842b02c10c6e38867e38370dc8d1666d591594
BalgoAlexo1 <129190812+BalgoAlexo1@users.noreply.github.com>
2024-05-28 04:16:24 +0200

    erreur de saisie correction

--- src/Model/Tuile.java
+++ src/Model/Tuile.java
@@ -328,7 +328,7 @@ public class Tuile {
             }
         }
         for (int k = 0; k < Plateau_de_jeu.baleines.size(); k++) {
-            if (Position.isPositionsEquals(position, Plateau_de_jeu.requins.get(k).getPosition())) {
+            if (Position.isPositionsEquals(position, Plateau_de_jeu.baleines.get(k).getPosition())) {
                 return false;
             }
         }
