7d1fa115023d489298203c444c8b3f2b26e6c892
IbrahimaDiop221 <82620658+IbrahimaDiop221@users.noreply.github.com>
2024-05-28 09:40:55 +0200

    new plateau

--- src/view/Filtre6.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-public class Filtre6 {
-
-    public void filtre_6(HexagonalGrid hexagonalGrid, String message) {
-        hexagonalGrid.showTemporaryMessage(message, 2000);
-    }
-}
